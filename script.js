0808572cd3ea96496b6df12c6b134351f02588d3
misbahunnura <misbahunnura650@gmail.com>
2026-06-16 14:56:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function goProfile() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("profil").style.display = "block";
+}
+
+function goBack() {
+    document.getElementById("profil").style.display = "none";
+    document.getElementById("home").style.display = "block";
+}
